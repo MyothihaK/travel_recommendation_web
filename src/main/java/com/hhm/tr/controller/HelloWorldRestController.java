971a3124858af05482bb56636b6df777aced0463
Han Hlaing <hanhlaingmoe.hhm27@gmail.com>
2018-10-09 22:30:41 +0630

    - change db name

--- src/main/java/com/hhm/tr/controller/HelloWorldRestController.java
+++ src/main/java/com/hhm/tr/controller/HelloWorldRestController.java
@@ -9,8 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.hhm.tr.model.Role;
 import com.hhm.tr.model.User;
 import com.hhm.tr.service.UserService;
 
